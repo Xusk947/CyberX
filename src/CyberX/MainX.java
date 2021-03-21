f7c550dcf0f5027e77e33aa80ba06e0966181485
Xusk <Xusk947@gmail.com>
2021-03-21 14:22:31 +0500

    oh no

--- src/CyberX/MainX.java
+++ src/CyberX/MainX.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package CyberX;
 
 import CyberX.content.Blocks;
@@ -18,7 +13,6 @@ public class MainX extends Mod {
     public ContentList[] content = new ContentList[] {
         new Blocks()
     };
-    
     public MainX() {
         
     }
--- src/CyberX/content/Blocks.java
+++ src/CyberX/content/Blocks.java
@@ -7,7 +7,6 @@ import mindustry.world.blocks.defense.turrets.LaserTurret;
 public class Blocks implements ContentList {
 
     public static Block test;
-    
     public void load() {
         test = new LaserTurret("ohno"){{
             reloadTime = 1f;
